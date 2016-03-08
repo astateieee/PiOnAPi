2bd0bf13f8f37253bcb1d2ec605b7ba6d515720d
Brandon Sims <brandon.w.sims@gmail.com>
2016-03-07 18:37:32 -0600

    Added some ASCII formatting

--- pi.py
+++ pi.py
@@ -2,14 +2,25 @@
 # To make this display indefinitely remove n < 50 and n += 1 and replace
 # n < 50 with while True
 
+from pyfiglet import figlet_format
+from termcolor import cprint
+from time import sleep
+
+pi = ""
 n = 0
 k, a, b, a1, b1 = 2, 4, 1, 12, 4
-while n < 50:
+while True:
 	p, q, k = k * k, 2 * k + 1, k + 1
 	a, b, a1, b1 = a1, b1, p * a + q * a1, p * b + q * b1
 	d, d1 = a / b, a1 / b1
 	while d == d1:
-		print(d)
-		a, a1 = 10 * (a % b), 10 * (a1 % b1)
-		d, d1 = a / b, a1 / b1
-		n += 1
+		pi += str(d)
+		if(n < 8):
+			a, a1 = 10 * (a % b), 10 * (a1 % b1)
+			d, d1 = a / b, a1 / b1
+			n += 1
+		else:
+			cprint(figlet_format(pi, font="starwars"))
+			pi = ""
+			n = 0
+			sleep(0.5)
